ece6c6ae9a6a3d895b02ced90e9b6cfa54c950cc
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2022-11-02 09:58:57 +0200

    first commit

---
+++ src/store/counter.js
@@ -0,0 +1,23 @@
+import { makeAutoObservable, makeObservable } from "mobx"
+
+class Counter {
+  count = 0;
+  timer = 60;
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  increment() {
+    this.count = this.count + 1;
+  }
+
+  decrement() {
+    this.count = this.count - 1;
+  }
+
+  get total() {
+    return `Count + timer = ${this.timer + this.count}`
+  }
+}
+
+export default new Counter()
